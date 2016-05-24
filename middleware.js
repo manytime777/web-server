3394fe70c09f44d3eb6ccaa99dc6fa831499c200
manytime777 <manytime@777.so-net.jp>
2016-05-24 16:44:55 +0900

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,14 @@
+//module.exports = {
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		// new Date().toString()
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+module.exports = middleware;
